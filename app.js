5405d7875dcf00dcb6245a836a2756068454a391
Elshibani Omar <amr.omar80@gmail.com>
2018-04-16 21:07:18 -0600

    Add app files

--- app.js
+++ app.js
@@ -227,7 +227,7 @@ const AppCtrl = (function(ItemCtrl, UICtrl) {
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
     // Edit icon click event
-    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
   }
 
   // Add item submit
@@ -258,8 +258,8 @@ const AppCtrl = (function(ItemCtrl, UICtrl) {
     e.preventDefault();
   }
 
-  // Update item submit
-  const itemUpdateSubmit = function(e) {
+  // Click edit item icon
+  const itemEditClick = function(e) {
 
     if(e.target.classList.contains('edit-item')) {
       // Get list item id (item-0, item-1)
